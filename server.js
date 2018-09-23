b5c5046e3732b16ad69f3a2475ee8bd3da4ee779
M1n007 <amin4udin@gmail.com>
2018-09-23 23:42:19 +0700

    add audio

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-// const line = require('@line/bot-sdk');
-// const express = require('express');
-// const axios = require('axios');
+const line = require('@line/bot-sdk');
+const express = require('express');
+const axios = require('axios');
  
-// const config = {
-//   channelAccessToken: "WieVSkkstB+KILW7ZqdpMKkPs+BzI4M3cEcjXyIrNe9ynVjJZKUd9XKveN5EeA24iabMnLB704xKwQZMD3RxKU+m/IZT2t/hJK/biFhJi4922QoIaksxgcjVaY2AEsOhPVJKbGfeMiQwR8HXgXgewdB04t89/1O/w1cDnyilFU=",
-//   channelSecret: "50609a42ec09ac881edf63a1ada5317c",
-// };
+const config = {
+  channelAccessToken: "WieVSkkstB+KILW7ZqdpMKkPs+BzI4M3cEcjXyIrNe9ynVjJZKUd9XKveN5EeA24iabMnLB704xKwQZMD3RxKU+m/IZT2t/hJK/biFhJi4922QoIaksxgcjVaY2AEsOhPVJKbGfeMiQwR8HXgXgewdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "50609a42ec09ac881edf63a1ada5317c",
+};
  
-// // create LINE SDK client
-// const client = new line.Client(config);
-// const app = express();
+// create LINE SDK client
+const client = new line.Client(config);
+const app = express();
  
-// // register a webhook handler with middleware
-// // about the middleware, please refer to doc
-// app.post('/callback', line.middleware(config), (req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.json(result))
-//     .catch((e)=>{
-//       console.log(e);
-//     });
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((e)=>{
+      console.log(e);
+    });
  
-// });
+});
  
-// function handleEvent(event) {
+function handleEvent(event) {
  
-//     if(event.message.text == "hai"){
-//       const echo = { type: 'text', text: "Halo juga :)·" };
-//       return client.replyMessage(event.replyToken, echo);
-//     }
+    if(event.message.text == "hai"){
+      const echo = { type: 'text', text: "Halo juga :)·" };
+      return client.replyMessage(event.replyToken, echo);
+    }
  
-//     const echo = { type: 'text', text: "Saya tidak mengerti, saya simpan dulu" };
-//     return client.replyMessage(event.replyToken, echo);
-// }
+    const echo = { type: 'text', text: "Saya tidak mengerti, saya simpan dulu" };
+    return client.replyMessage(event.replyToken, echo);
+}
  
-// // listen on port
-// const port = 3000;
-// app.listen(port, () => {
-//   console.log(`listening on ${port}`);
-// });
+// listen on port
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
--- tele_haru.js
+++ tele_haru.js
@@ -1,8 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '659199832:AAFs-MyQ-u0N3sCbeSJFczW6-ZxN_GEvNxs';
 const bot = new TelegramBot(token, { polling: true });
+const fetch = require('node-fetch');
+const request = require('request');
+
+const simsimiKey = 'cc09e04a-62db-492d-b3b1-4ec654ed82ed';
+const url = `http://sandbox.api.simsimi.com/request.p?key=${simsimiKey}&lc=id&ft=1.0&text=`;
+
+// async function getSholat(){
+//     await fetch('http://muslimsalat.com/jakarta.json?key=bd099c5825cbedb9aa934e255a81a5fc')
+//             .then(resp => resp.json())
+//             .then(respo => {
+//                 respo.items[0]
+//             })
+// }
+
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, msg.text.toString());
+
+    request(url + msg.text.toString(), (err, response, body) => {
+        bot.sendMessage(chatId, JSON.parse(body).response);
+    });
+
+    // if (msg.text.toString() == "jadwal sholat") {
+    //     bot.sendMessage(chatId, "ini jadwal sholat sekarang ka "+JSON.parse(getSholat()));
+    // }
+
+    if (msg.text.toString() === "/start") {
+        bot.sendMessage(chatId, "Terimakasih telah menambahkan ku sebagai teman ^_^, semoga aku bisa membuat hari harimu lebih menyenangkan.");
+        bot.sendAudio(chatId, 'haru.mp3')
+    }
 });
+
