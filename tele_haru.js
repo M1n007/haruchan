48e6c8f6d06d7172fe8b806e9ff0a2e6233ed939
M1n007 <amin4udin@gmail.com>
2018-09-24 00:06:55 +0700

    update word

--- tele_haru.js
+++ tele_haru.js
@@ -1,10 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '659199832:AAFs-MyQ-u0N3sCbeS]';
+const token = '659199832:AAFs-MyQ-u0N3sCbeSJFczW6-ZxN_GEvNxs';
 const bot = new TelegramBot(token, { polling: true });
 const fetch = require('node-fetch');
 const request = require('request');
 
-const simsimiKey = 'cc09e04a-62db-492d-b3b1';
+const simsimiKey = 'cc09e04a-62db-492d-b3b1-4ec654ed82ed';
 const url = `http://sandbox.api.simsimi.com/request.p?key=${simsimiKey}&lc=id&ft=1.0&text=`;
 
 // async function getSholat(){
@@ -26,6 +26,10 @@ bot.on('message', (msg) => {
     // if (msg.text.toString() == "jadwal sholat") {
     //     bot.sendMessage(chatId, "ini jadwal sholat sekarang ka "+JSON.parse(getSholat()));
     // }
+
+    if (msg.text.toString() === "namanya siapa") {
+        bot.sendMessage(chatId, "namaku haruchan, calonnya aminudin hehe");
+    }
 
     if (msg.text.toString() === "/start") {
         bot.sendMessage(chatId, "Terimakasih telah menambahkan ku sebagai teman ^_^, semoga aku bisa membuat hari harimu lebih menyenangkan.");
